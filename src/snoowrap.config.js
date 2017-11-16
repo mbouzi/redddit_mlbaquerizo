813834377911190e51ee36e260903a5895bd64b3
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2017-11-16 16:57:28 -0500

    Remove api credentials

--- src/snoowrap.config.js
+++ src/snoowrap.config.js
@@ -1,8 +1,8 @@
 export default
 {
-  userAgent: 'redddit_mlbaquerizo',
-  clientId: '6UXKDzqZAQbt0Q',
-  clientSecret: 'm8HmFKvLULgtSc3GOkzGS6C-zio',
-  username: 'redddit_mlbaquerizo',
-  password: 'SMMk*"j6_5)nM};+'
+  userAgent: '',
+  clientId: '',
+  clientSecret: '',
+  username: '',
+  password: ''
 };
